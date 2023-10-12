b823dd41f12095f70518da46589e88a305cebe60
Bastian Nazaromi <bastian.nazaromi@gmail.com>
2023-10-12 08:24:14 +0700

    first commit

---
+++ login.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>Halaman Login</title>
+
+	<link rel="stylesheet" type="text/css" href="src/login/style.css">
+</head>
+
+<body>
+	<h2>Form Login</h2>
+	<form action="prosesLogin.php" method="POST" id="loginForm">
+		<label for="username">Username</label><br>
+		<input type="text" id="username" name="username" required autocomplete="off"><br><br>
+
+		<label for="password">Password</label><br>
+		<input type="password" id="password" name="password" required autocomplete="off"><br><br>
+
+		<button type="button" id="loginButton">Login</button>
+	</form>
+
+	<script src="src/login/script.js"></script>
+</body>
+
+</html>
